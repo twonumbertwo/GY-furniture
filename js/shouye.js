fb71a67a210880844fd3cdd7095af88d1e3b4534
lining999 <634696359@qq.com>
2017-12-12 10:42:22 +0800

    lunbo

--- js/shouye.js
+++ js/shouye.js
@@ -1,32 +1,29 @@
 window.onload=function(){
 	
-		setInterval(function(){
-			$("#likuang").animate({
-				marginLeft:"-400px"
-				
-			},1000,function(){
-				
-			$("img:last").after($("img:first"))
-			   $("#likuang").css({
-				"margin-left":"0px"
-				
-			   })
-			})
-			
-			
-			
-			
-			
-		},2000)
+	
+	
+	
+	
+	
+			    n = 0;
+
+
+				function q() {
+					n++;
+					if(n > 4) {
+						n = 0;
+						$("#likuang").css({
+							"margin-left": "0px"
+						});
+					}
+					$("#likuang").animate({
+						marginLeft: -1583 * n
+					}, 2000);
+
+
+				}
+				time = setInterval(q, 3000);
 		
-		
-		
-		
-	
-	
-	
-	
-	
-	
+
 	
 }
